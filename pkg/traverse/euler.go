2a7dd15f3fd224a21eca174c2ef6129f9ed39f53
Gabriel Barrientos <gabri12.rubik@gmail.com>
2024-10-17 16:18:04 -0300

    Agregar aristas para recorrer el grafo con Euler

--- pkg/traverse/euler.go
+++ pkg/traverse/euler.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"graph/pkg/collections"
 	"graph/pkg/graph"
+	"math"
+	"slices"
 )
 
 var (
@@ -63,6 +65,32 @@ func (p Pair) Different(other Pair) bool {
 	return p.L.Id != other.L.Id && p.L.Id != other.R.Id && p.R.Id != other.L.Id && p.R.Id != other.R.Id
 }
 
+// sort the nodes in the pair so they're consistent
+func (p *Pair) Normalize() {
+	if p.L.Id > p.R.Id {
+		p.L, p.R = p.R, p.L
+	}
+}
+
+// normalize the pairing, sort each pair and then the slice
+func normalizePairing(pairing []Pair) {
+	for i := range pairing {
+		pairing[i].Normalize() // normalize each pair
+	}
+	// sort the pairing
+	slices.SortFunc(pairing, func(a, b Pair) int {
+		if a.Key() < b.Key() {
+			return -1
+		} else {
+			return 1
+		}
+	})
+}
+
+func (p Pair) Key() string {
+	return fmt.Sprintf("%s|%s", p.L.Id, p.R.Id)
+}
+
 func generatePairing(pair Pair, pairs []Pair) []Pair {
 	pairing := make([]Pair, 0)
 
@@ -99,7 +127,79 @@ func generateAllPairings(pairs []Pair) [][]Pair {
 	return pairings
 }
 
-func eulerizeGraph(g *graph.Graph) {
+func removeDuplicatePairings(pairings [][]Pair) [][]Pair {
+	uniquePairings := make([][]Pair, 0)
+	seen := make(map[string]bool)
+
+	for _, pairing := range pairings {
+		normalizePairing(pairing)
+		key := fmt.Sprint(pairing)
+		if !seen[key] {
+			seen[key] = true
+			uniquePairings = append(uniquePairings, pairing)
+		}
+	}
+
+	return uniquePairings
+}
+
+func getBestPairing(g graph.Graph, pairings [][]Pair) ([]Pair, error) {
+	if len(pairings) == 0 {
+		return []Pair{}, errors.New("no pairings to compare")
+	}
+
+	// keep track of the best pairing so far
+	bestPairingId := 0
+	bestPairingWeight := math.MaxInt
+
+	for pairingId, pairing := range pairings {
+		// calculate weight for the pairing
+		pairingWeight := 0
+		for _, pair := range pairing {
+			s, err := Dijkstra(g, pair.L, pair.R)
+			if err != nil {
+				return []Pair{}, err
+			}
+			pairingWeight += s.Distance
+		}
+
+		// update best pairing if necessary
+		if pairingWeight < bestPairingWeight {
+			bestPairingWeight = pairingWeight
+			bestPairingId = pairingId
+		}
+	}
+
+	return pairings[bestPairingId], nil
+}
+
+func duplicateEdges(g *graph.Graph, pairing []Pair) error {
+	for _, pair := range pairing {
+		// get all nodes to connect the pair
+		sequence, err := Dijkstra(*g, pair.L, pair.R)
+		if err != nil {
+			return err
+		}
+
+		// add edges to connect the pair again
+		for i := 1; i < len(sequence.Sequence); i++ {
+			a := sequence.Sequence[i-1]
+			b := sequence.Sequence[i]
+			edge, ok := g.GetShortestEdge(a, b)
+			if !ok {
+				return fmt.Errorf("couldn't get shortest edge between %v and %v", a.Id, b.Id)
+			}
+			newEdge := graph.NewEdge(a, b, edge.Weight)
+			err = g.AddEdge(newEdge)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func eulerizeGraph(g *graph.Graph) error {
 	// duplicate edges of deadend nodes
 	deadendNodes := g.GetAllDeadendNodes()
 	for _, deadendNode := range deadendNodes {
@@ -112,22 +212,48 @@ func eulerizeGraph(g *graph.Graph) {
 
 	// create the different ways to join the odd nodes without repetition
 	allPairings := generateAllPairings(pairs)
-	fmt.Printf("pairings: %v\n", allPairings)
+
+	// remove duplicate pairings
+	uniquePairings := removeDuplicatePairings(allPairings)
+
+	// get best pairing
+	bestPairing, err := getBestPairing(*g, uniquePairings)
+	if err != nil {
+		return err
+	}
+
+	// duplicate necessary edges
+	err = duplicateEdges(g, bestPairing)
+	if err != nil {
+		return err
+	}
+
+	g.Print()
+
+	return nil
 }
 
 func Euler(g graph.Graph, a graph.Node) (Sequence, error) {
 	// clone to avoid modifying the original graph
-	g = g.Clone()
+	h := g.Clone()
+
+	// check that starting node exists
+	_, err := h.GetNode(a.Id)
+	if err != nil {
+		return Sequence{}, err
+	}
 
 	// check if graph is Eulerian
-	if !isEulerianGraph(g) {
-		eulerizeGraph(&g)
-		return Sequence{}, errors.New(ErrGraphNotEulerian)
+	if !isEulerianGraph(h) {
+		err := eulerizeGraph(&h)
+		if err != nil {
+			return Sequence{}, err
+		}
 	}
 
 	// setup initial values
 	es := newEulerState()
-	edges := g.GetAllEdges()
+	edges := h.GetAllEdges()
 	for _, edge := range edges {
 		es.visitedEdges[edge.Key()] = false
 	}
@@ -139,7 +265,7 @@ func Euler(g graph.Graph, a graph.Node) (Sequence, error) {
 	x := a
 	for !es.allEdgesVisited() {
 		// get valid edges to go to the next node
-		edges := g.GetEdges(x)
+		edges := h.GetEdges(x)
 		validEdges := make([]graph.Edge, 0, len(edges))
 		for _, edge := range edges {
 			if !es.visitedEdges[edge.Key()] {
@@ -159,7 +285,7 @@ func Euler(g graph.Graph, a graph.Node) (Sequence, error) {
 			es.invalidEdges[key] = true
 
 			// mark the edge as not visited again
-			edge, ok := g.GetShortestEdge(previousNode, invalidNode)
+			edge, ok := h.GetShortestEdge(previousNode, invalidNode)
 			if !ok {
 				return Sequence{}, fmt.Errorf("couldn't get shortest edge between %v and %v", previousNode.Id, invalidNode.Id)
 			}
@@ -189,7 +315,7 @@ func Euler(g graph.Graph, a graph.Node) (Sequence, error) {
 	for i := 0; i < len(s.Sequence)-1; i++ {
 		a := s.Sequence[i]
 		b := s.Sequence[i+1]
-		edge, ok := g.GetShortestEdge(a, b)
+		edge, ok := h.GetShortestEdge(a, b)
 		if !ok {
 			return Sequence{}, fmt.Errorf("couldn't get shortest edge between %v and %v", a.Id, b.Id)
 		}
